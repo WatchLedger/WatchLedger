d9a31c9578b97e6812f2cd3ea4f5aee6df0d2986
NathanGeleyn <nathan.geleyn@student.hogent.be>
2025-12-01 21:14:29 +0100

    Just small documentation changes to add roles and stuff for future reference

--- WatchPlatform/WatchCollection.Api/Controllers/WatchController.cs
+++ WatchPlatform/WatchCollection.Api/Controllers/WatchController.cs
@@ -10,10 +10,13 @@ namespace WatchCollection.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //[Authorize]
     public class WatchController(IWatchService _service) : ControllerBase
     {
         
         [HttpPost]
+        //only possible for logged in users and admins
+        //[RoleAuthorize("User", "Admin")]
         public async Task<ActionResult<WatchResponseContract>> CreateWatch([FromBody] WatchRequestContract contract)
         {
             try
@@ -29,6 +32,8 @@ namespace WatchCollection.Api.Controllers
 
         [HttpGet]
         [Route("{watchId:Guid}")]
+        // only possible for logged in users and admins
+        //[RoleAuthorize("User", "Admin")]
         public async Task<ActionResult<WatchResponseContract>> GetWatchById([FromRoute] Guid watchId)
         {
             try
@@ -45,6 +50,8 @@ namespace WatchCollection.Api.Controllers
         }
 
         [HttpGet]
+        // only possible for logged in users and admins
+        //[RoleAuthorize("User", "Admin")]
         public async Task<IActionResult> GetAllWatches([FromQuery] string? brand = null)
         {
             try
@@ -63,6 +70,8 @@ namespace WatchCollection.Api.Controllers
 
         [HttpPut]
         [Route("{watchId:Guid}")]
+        // only possible for logged in users and admins
+        //[RoleAuthorize("User", "Admin")]
         public async Task<ActionResult<WatchResponseContract>> Update([FromRoute] Guid watchId, [FromBody] WatchRequestContract contract)
         {
             try
@@ -82,6 +91,8 @@ namespace WatchCollection.Api.Controllers
 
         [HttpDelete]
         [Route("{watchId:Guid}")]
+        // only possible for logged in users and admins
+        //[RoleAuthorize("User", "Admin")]
         public async Task<ActionResult> DeleteWatch([FromRoute] Guid watchId)
         {
             try
